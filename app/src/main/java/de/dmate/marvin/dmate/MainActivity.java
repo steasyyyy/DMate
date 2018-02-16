182d97fd618656d14d3aca1beac4480e6d2a5e95
Marvin <marvin.reese@web.de>
2018-02-16 18:17:58 +0100

    code cleanup

--- app/src/main/java/de/dmate/marvin/dmate/MainActivity.java
+++ app/src/main/java/de/dmate/marvin/dmate/MainActivity.java
@@ -11,8 +11,6 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -20,13 +18,10 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -47,17 +42,12 @@ public class MainActivity extends AppCompatActivity {
         Helper.getInstance().setApplication(app);
 
 
-
-
-
-
-
 //        testing stuff
-        app.resetAllPrefs();
-        Entry a = Entry.bolus(7f).build();
-        Entry b = Entry.bloodsugar(100).breadunit(15.5f).bolus(31f).build();
-        Entry c = Entry.bloodsugar(127).note("TestNote").basal(18f).bolus(27f).breadunit(13.5f).build();
-        Entry d = Entry.bloodsugar(111).basal(7f).breadunit(7.5f).bolus(15f).note("Nooooooote").build();
+//        app.resetAllPrefs();
+//        Entry a = Entry.bolus(7f).build();
+//        Entry b = Entry.bloodsugar(100).breadunit(15.5f).bolus(31f).build();
+//        Entry c = Entry.bloodsugar(127).note("TestNote").basal(18f).bolus(27f).breadunit(13.5f).build();
+//        Entry d = Entry.bloodsugar(111).basal(7f).breadunit(7.5f).bolus(15f).note("Nooooooote").build();
     }
 
 }
