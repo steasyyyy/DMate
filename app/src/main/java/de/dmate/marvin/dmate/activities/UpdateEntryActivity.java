928e5a61f3f8b041224ae6e3f175e82b6c4069df
Marvin <marvin.reese@web.de>
2018-02-17 17:50:30 +0100

    working on the layout for updating an entry

---
+++ app/src/main/java/de/dmate/marvin/dmate/activities/UpdateEntryActivity.java
@@ -0,0 +1,15 @@
+package de.dmate.marvin.dmate.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.dmate.marvin.dmate.R;
+
+public class UpdateEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_entry);
+    }
+}
