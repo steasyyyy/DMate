935e1eb0ec0cab96d091e7086dc3f1513682ef7f
Marvin <marvin.reese@web.de>
2018-06-17 15:24:37 +0200

    rebuild

--- app/src/main/java/de/dmate/marvin/dmate/fragments/SettingsDialogFragments/TargetAreaDialogFragment.java
+++ app/src/main/java/de/dmate/marvin/dmate/fragments/SettingsDialogFragments/TargetAreaDialogFragment.java
@@ -1,9 +1,12 @@
 package de.dmate.marvin.dmate.fragments.SettingsDialogFragments;
 
 import android.app.Dialog;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
@@ -11,12 +14,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
 
 import de.dmate.marvin.dmate.R;
+import de.dmate.marvin.dmate.roomDatabase.DataViewModel;
+import de.dmate.marvin.dmate.roomDatabase.Entities.User;
 
 public class TargetAreaDialogFragment extends DialogFragment {
 
     private OnTargetAreaDialogFragmentInteractionListener mListener;
+
+    private EditText editTextMin;
+    private EditText editTextMax;
+    private Button buttonConfirm;
+
+    private DataViewModel viewModel;
+    private User user;
 
     public TargetAreaDialogFragment() {
         // Required empty public constructor
@@ -58,6 +75,47 @@ public class TargetAreaDialogFragment extends DialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dialog_target_area, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        editTextMin = view.findViewById(R.id.editText_target_min);
+        editTextMax = view.findViewById(R.id.editText_target_max);
+        buttonConfirm = view.findViewById(R.id.button_confirm_target_area);
+
+        viewModel = ViewModelProviders.of(this).get(DataViewModel.class);
+
+        viewModel.getUsers().observe(TargetAreaDialogFragment.this, new Observer<List<User>>() {
+            @Override
+            public void onChanged(@Nullable final List<User> users) {
+//                initialize user
+//                if user exists, initialize EditTexts with values from DB
+//                if user does not exist, create new user
+                try {
+                    user = users.get(0);
+                    if (user.getTargetMin() != null) editTextMin.setText(user.getTargetMin().toString());
+                    if (user.getTargetMax() != null) editTextMax.setText(user.getTargetMax().toString());
+                } catch (IndexOutOfBoundsException e) {
+                    user = new User();
+                    viewModel.addUser(user);
+                    Toast toast = Toast.makeText(getContext(), "Created new user", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+
+        buttonConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.setTargetMin(Integer.parseInt(editTextMin.getText().toString()));
+                user.setTargetMax(Integer.parseInt(editTextMax.getText().toString()));
+                viewModel.addUser(user);
+                dismiss();
+            }
+        });
+
+        super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
